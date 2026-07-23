447956454267f899645ce772d7181cb13589b186
Kerry Lugar <kerrylugar26@gmail.com>
2026-07-23 10:20:58 +0800

    Change email placeholder

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -55,7 +55,7 @@ export default function LoginPage() {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="kerry@taskflow.com"
+              placeholder="Email"
               required
             />
           </div>
